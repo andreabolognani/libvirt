ff03996205507f768710ddb623e862b1f98b4f29
Andrea Bolognani <eof@kiyuko.org>
2019-06-16 16:15:37 +0200

    update sample program

--- src/nib/src/main.rs
+++ src/nib/src/main.rs
@@ -1,6 +1,16 @@
 extern crate nib;
 
+use nib::devices::Memballoon;
+use nib::devices::MemballoonModel;
+
 fn main() {
     let file_name = nib::util::build_file_name("guest", ".xml");
     println!("{}", file_name);
+
+    let mut memballoon = Memballoon::new();
+
+    println!("{}", memballoon.to_str());
+
+    memballoon.set_model(MemballoonModel::VirtIO);
+    println!("{}", memballoon.to_str());
 }
